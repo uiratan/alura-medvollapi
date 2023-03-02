9ffc4e242e8b4321eeed85be960ff1bdb1a207b5
uiratan <uiratan@gmail.com>
2023-03-02 00:07:27 -0300

    listagem de medicos com DTO restringindo as informações listadas

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,9 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +25,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+//    public Page<DadosListagemMedico> listar(@PageableDefault(size=10, page=0, sort={"nome"}) Pageable pageable) {
+    public Page<DadosListagemMedico> listar(@PageableDefault(sort={"nome"}) Pageable pageable) {
+        //return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+        return repository.findAll(pageable).map(DadosListagemMedico::new);
     }
 }
