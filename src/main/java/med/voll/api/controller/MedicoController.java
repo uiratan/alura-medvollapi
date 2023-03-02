c2f896105a53b0a10fda81f9270df2f0a0a60271
uiratan <uiratan@gmail.com>
2023-03-02 04:47:48 -0300

    atualização para ResponseEntity e criado detalhamento de medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -20,35 +22,64 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dados);
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+
+        // devolver 201
+        // devolver o cabeçalho location com a uri
+        // devolver no corpo da resposta uma representação do recurso recem criado
+
     }
 
-
-//    public Page<DadosListagemMedico> listar(@PageableDefault(size=10, page=0, sort={"nome"}) Pageable pageable) {
-
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(sort={"nome"}) Pageable pageable) {
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(sort={"nome"}) Pageable pageable) {
         //return repository.findAll().stream().map(DadosListagemMedico::new).toList();
         // busca todos os medicos
         //return repository.findAll(pageable).map(DadosListagemMedico::new);
 
         // busca apenas os medicos com ativo = true usando padrao do spring - by campo valor
-        return repository.findAllByAtivoTrue(pageable).map(DadosListagemMedico::new);
+        var Page = repository.findAllByAtivoTrue(pageable).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(Page);
+
+        // retorna 200
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        // Hard delete  repository.deleteById(id);
+        // Exclusao lógica
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+
+        //retorna 200
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosMedicoAtualizacao dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosMedicoAtualizacao dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+
+        //retorna 200
     }
-    @DeleteMapping("{id}")
+
+    @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         // Hard delete  repository.deleteById(id);
         // Exclusao lógica
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
+
+        // retorna 204
     }
 }
